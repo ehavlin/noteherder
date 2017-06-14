583d39d2972c3a14ffe97705df944da722df69e8
Evan Havlin <ehavlin@hotmail.com>
2017-06-14 17:06:21 -0400

    Add content and style to components

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,7 +4,18 @@ import './NoteForm.css'
 
 class NoteForm extends Component {
     render() {
-        return <p>NOTE FORM!</p>
+        return (
+                <div className="NoteForm">
+                    <form>
+                    <p>
+                        <input type="text" name="title" placeholder="Title your note" value="" />
+                    </p>
+                    <p>
+                        <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                    </p>
+                    </form>
+                </div>
+                )
     }
 }
 
