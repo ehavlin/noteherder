e258e48b91cc97d592d1b9de28167bb8b13c1e4c
Evan Havlin <ehavlin@hotmail.com>
2017-06-14 21:54:26 -0400

    Grab data from form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,17 +3,42 @@ import React, { Component } from 'react'
 import './NoteForm.css'
 
 class NoteForm extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            title: '',
+            body: '',
+        }
+
+        this.updateBody = this.updateBody.bind(this)
+    }
+
+    updateBody(event) {
+        this.setState({
+            body: event.target.value
+        })
+    }
+
+    addNote(event) {
+        event.preventDefault()
+        const title = this.titleInput.value
+        const body = this.state.body
+        //Have to push these values onto array in NoteList
+    }
+
     render() {
         return (
                 <div className="NoteForm">
                     <form>
                     <p>
-                        <input type="text" name="title" placeholder="Title your note" value="" />
+                        <input type="text" name="title" placeholder="Title your note" ref={input => this.titleInput = input} />
                     </p>
                     <p>
-                        <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                        <textarea name="body" cols="30" rows="10" placeholder="Just start typing..." value={this.state.body} onChange={this.updateBody}></textarea>
                     </p>
                     </form>
+                    <button onClick={this.addNote.bind(this)} className="button">Submit Note</button>
                 </div>
                 )
     }
